691b4c947f9ef9997767294589295e133eb8b552
kanwar sain <kanwarsingh0507@gmail.com>
2026-05-22 12:11:41 +0530

    captain login UI improves

--- frontend/src/pages/CaptainLogin.jsx
+++ frontend/src/pages/CaptainLogin.jsx
@@ -2,7 +2,9 @@ import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { CaptainDataContext } from "../context/CapatinContext";
 import { useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
 import axios from "axios";
+import Navbar from "../components/Navbar";
 
 const CaptainLogin = () => {
   const [email, setEmail] = useState("");
@@ -17,7 +19,7 @@ const CaptainLogin = () => {
     try {
       const response = await axios.post(
         `${import.meta.env.VITE_BASE_URL}/captains/login`,
-        captainData
+        captainData,
       );
 
       if (response.status === 200) {
@@ -34,66 +36,81 @@ const CaptainLogin = () => {
     setPassword("");
   };
   return (
-    <div className="p-7 flex flex-col justify-between h-screen">
-      <div className="bg-white w-full flex flex-col ">
-        <div className="text-4xl text-black mb-5 font-medium">Uber</div>
+    <div className="min-h-screen text-white flex flex-col justify-between bg-[radial-gradient(circle_at_top_left,#0f172a,#020617)] overflow-hidden relative p-6 box-border">
+      <Navbar />
+      <div className="relative z-10 w-full max-w-[380px]">
         <form
           onSubmit={(e) => {
             submitHandler(e);
           }}
-          className=""
+          className="flex flex-col items-center justify-center bg-slate-900/70 p-4 py-6 rounded-xl backdrop-blur-sm border border-white/20 shadow-[0_0_40px_rgba(0,0,0,0.6)]"
         >
-          <div className="flex flex-col gap-2">
-            <label className="text-xl font-semibold mb-2 " htmlFor="email">
-              What's Your Email
-            </label>
+          <h2 className="text-center text-white  text-2xl font-semibold">
+            Welcome Back
+          </h2>
+          <p className="text-sm mb-10  text-lime-500/70">
+            Login to earn from rides
+          </p>
+          <label
+            htmlFor="email"
+            className="self-start text-lg text-gray-200 p-2 "
+          >
+            E-mail
+          </label>
+          <div className="flex flex-row gap-2 w-full border border-white/10  px-4 py-3 rounded-xl bg-slate-800 backdrop-blur shadow-[0_0_40px_rgba(0,0,0,0.6)] mb-2">
+            <i className="text-xl text-lime-500/70 font-medium ri-mail-open-line"></i>
             <input
-              className="bg-[#eeeeee] py-2 px-4 border-1 mb-3 rounded placeholder:text-base "
+              className="outline-none placeholder:text-base w-full "
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               type="email"
               id="email"
               required
-              placeholder="email@example.com"
+              placeholder="abc@example.com"
             />
           </div>
-          <div className="flex flex-col gap-2 mt-4">
-            <label className="text-xl font-semibold mb-2 " htmlFor="password">
-              Password
-            </label>
+          {/* password */}
+          <label
+            htmlFor="password"
+            className="self-start text-lg text-gray-200 p-2 "
+          >
+            Password
+          </label>
+          <div className="flex flex-row gap-2 w-full border border-white/10  px-4 py-3 rounded-xl bg-slate-800 backdrop-blur shadow-[0_0_40px_rgba(0,0,0,0.6)]">
+            <i className="text-xl text-lime-500/70 font-medium ri-lock-2-line"></i>
             <input
-              className="bg-[#eeeeee] py-2 px-4 border-1 mb-3 rounded placeholder:text-base"
+              className="outline-none placeholder:text-base "
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               type="password"
               id="password"
               required
-              placeholder="Enter your password"
+              placeholder="••••••••"
             />
           </div>
+          <button className="self-start text-sm text-lime-500/70 pl-2 mt-2">
+            Forgot Password ?
+          </button>
+
           <button
             type="submit"
-            className="bg-black w-full text-center text-white text-xl mb-3 py-2 px-4 rounded-lg flex items-center justify-center mt-6"
+            className="w-full bg-lime-500 text-black font-semibold backdrop-blur py-3 px-4 rounded-xl mt-8 "
           >
             Login
           </button>
+
+          <button className="w-full bg-slate-700 text-white/80 font-semibold py-2 px-4 rounded-xl mt-4 flex items-center justify-center gap-3 ">
+            <FcGoogle className="h-8 w-8" /> <span>Continue with Google</span>
+          </button>
         </form>
-        <p className="text-center">
-          Join new fleet?{" "}
-          <Link to="/captain-signup" className="text-blue-600 ">
-            Register as a Captain
-          </Link>
-        </p>
       </div>
 
-      <div className="">
-        <Link
-          to="/user-login"
-          className="bg-[#E37434] w-full text-center text-white text-xl  py-2 px-4 rounded-lg flex items-center justify-center mt-6"
-        >
-          Login as a User
+      <p className="text-center mt-4 text-slate-400 text-sm mb-4 ">
+        Don't have an account?{" "}
+        <Link to="/captain-signup" className="text-lime-500 hover:underline">
+          Sign up
         </Link>
-      </div>
+      </p>
     </div>
   );
 };
